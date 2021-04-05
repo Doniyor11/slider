4405a22c05eb9c967e69699632f2e65d35095e10
XasanDeveloper <MirKhasan96@yandex.ru>
2021-04-05 14:11:01 +0500

    Slider

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,7 @@
+import {Swiper, Parallax, Mousewheel,} from 'swiper'
+
+Swiper.use([Parallax, Mousewheel])
+
 // // Import jQuery module (npm i jquery)
 // import $ from 'jquery'
 // window.jQuery = $
@@ -6,8 +10,19 @@
 // // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
 
+
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+    // Custom JS
+
+
+    const swiperIMG = new Swiper('.slider-img', {
+        loop: false,
+        speed: 2400,
+        parallax: true,
+        mousewheel:{
+            invert: true,
+        }
+    });
 
 })
